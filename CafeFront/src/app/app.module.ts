67641fd307b75be78eb5a7de50ac9ab62e2765fc
ohoh00 <ohoh2425@hotmail.com>
2020-06-11 18:05:23 +0700

    เพิ่มเก็บข้อมูล managemenu

--- CafeFront/src/app/app.module.ts
+++ CafeFront/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from "@angular/common/http"
-import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule,FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -44,7 +44,8 @@ import { CustomerComponent } from './pages/customer/customer.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CafeFront/src/app/pages/managemenu/managemenu.component.ts
+++ CafeFront/src/app/pages/managemenu/managemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-managemenu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagemenuComponent implements OnInit {
 
+  manageForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    type: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    img: new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
