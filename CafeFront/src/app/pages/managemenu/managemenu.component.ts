508557055ca3d3669a5b542dd2e01f8c8b976db4
ohoh00 <ohoh2425@hotmail.com>
2020-06-14 22:02:15 +0700

    fix URL menu

--- CafeFront/src/app/pages/managemenu/managemenu.component.ts
+++ CafeFront/src/app/pages/managemenu/managemenu.component.ts
@@ -15,11 +15,69 @@ export class ManagemenuComponent implements OnInit {
     img: new FormControl('', [Validators.required])
   });
 
+<<<<<<< Updated upstream
   constructor() { }
+=======
+  menus:any
+
+  previewLoaded:boolean = false
+
+  constructor(private ms: MenuService) {
+    this.onLoading();
+   }
+>>>>>>> Stashed changes
 
   ngOnInit(): void {
   }
 
+<<<<<<< Updated upstream
   
+=======
+  addMenu(){
+    this.ms.addMenu(this.manageForm.value).subscribe(
+      data => {
+        console.log(data)
+        alert('Menu added successfully');
+        this.manageForm.reset();
+      },
+      err =>{
+        console.log(err);
+      });
+  }
+
+  onChangeImg(e:any){
+    if(e.target.files.length > 0) {
+      const file = e.target.files[0];
+      var pattern = /image-*/;
+      const reader = new FileReader();
+      if(!file.type.match(pattern)){
+        alert('invalid format');
+        this.manageForm.reset();
+      }else{
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          this.previewLoaded = true;
+          this.manageForm.patchValue({
+            img: reader.result
+          });
+        };
+      }
+    }
+  }
+
+  onLoading() {
+    try {
+      this.ms.getAllMenu().subscribe(
+        data => {
+          this.menus = data;
+      },
+        err => {
+          console.log(err)
+        });
+    } catch (error) {
+        console.log(error)
+    }
+  }
+>>>>>>> Stashed changes
 
 }
