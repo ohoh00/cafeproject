5e4a4005412f82bd098d29d1433ab53e43625bdb
apower2499 <apower2499@gmail.com>
2020-06-16 17:30:59 +0700

    regSHOP

--- CafeFront/src/app/pages/regshop/regshop.component.ts
+++ CafeFront/src/app/pages/regshop/regshop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ShopService } from '../../service/shop.service'
 
 @Component({
   selector: 'app-regshop',
@@ -7,9 +9,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegshopComponent implements OnInit {
 
-  constructor() { }
+  shopForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    imageProfile: new FormControl('https://cdn.onlinewebfonts.com/svg/img_148071.png', [Validators.required]),
+    time: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    tumbon: new FormControl('', [Validators.required]),
+    amphoe: new FormControl('', [Validators.required]),
+    province: new FormControl('', [Validators.required]),
+    post: new FormControl('', [Validators.required])
+  });
+
+  shops:any
+
+  constructor(private sh: ShopService) {
+    this.onLoading();
+   }
 
   ngOnInit(): void {
+  }
+
+  addShop(){
+    this.sh.addShop(this.shopForm.value).subscribe(
+      data => {
+        console.log(data)
+        alert('Shop added successfully');
+        this.onLoading();
+        this.resetForm();
+      },
+      err =>{
+        console.log(err);
+      });
+  }
+
+  onChangeImg(e:any){
+    if(e.target.files.length > 0) {
+      const file = e.target.files[0];
+      var pattern = /image-*/;
+      const reader = new FileReader();
+      if(!file.type.match(pattern)){
+        alert('invalid format');
+        this.shopForm.reset();
+      }else{
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          this.shopForm.patchValue({
+            imageProfile: reader.result
+          });
+        };
+      }
+    }
+  }
+
+  onLoading() {
+    try {
+      this.sh.getAllShops().subscribe(
+        data => {
+          this.shops = data;       
+      },
+        err => {
+          console.log(err)
+        });
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  resetForm(){
+    this.shopForm.reset();
   }
 
 }
